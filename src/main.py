5b0efb168807f7879c727a5a35e2c02fc060beec
Tim Jelenz <jelenzt@gmail.com>
2026-05-24 12:17:21 +0200

    add: static files
    - add `chat_page.html`, `styles.css` and `hands.png` into `static` directory

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from src.chat.chat_websockets import ChatCommunication
 from src.chat.http_server import HTTPServer
 import asyncio
+from os import listdir
 
 async def run():
     # Making Server Object
@@ -8,7 +9,7 @@ async def run():
     chat_server = ChatCommunication()
 
     # getting chat page
-    with open("chat_page.html", "r") as chat_page:
+    with open("src/chat/static/chat_page.html", "r") as chat_page:
         chat_start_page = chat_page.read()
 
     # Defining my Routes
